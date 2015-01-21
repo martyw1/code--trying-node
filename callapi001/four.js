e8eabd3124dc5fe32967d3b4a82c0ec71fb66973
martyw1 <martinwolfe@gmail.com>
2015-01-20 20:00:20 -0500

    still working to parse JSON

--- callapi001/four.js
+++ callapi001/four.js
@@ -3,53 +3,75 @@ var http = require('http');
 var optionsget = {
     host : 'api.opencorporates.com',
     port : 80,
-    path : '/v0.2/companies/search?q=barclays+bank&jurisdiction_code=gb',
+    path : '/v0.3/companies/search?q=barclays+bank&jurisdiction_code=gb',
     method : 'GET'
 };
 
 
-console.info('\n==============================================')
-console.info('\nStep 1: Options prepared:');
+console.info('\n==============================================');
+console.info('\ndebug - Step 1: Options prepared:');
 console.info(optionsget);
-console.info('\n==============================================')
-console.info('\nStep 2: Do the GET call');
-console.info('\n==============================================')
+console.info('\n==============================================');
+console.info('\ndebug - Step 2: Do the GET call');
+console.info('\n==============================================');
 
 var reqGet = http.get(optionsget, function(res) {
 
     // display some standard http data coming from the http 'GET' request.. happening as the stream comes in..
-    console.info("..now we are in the GET request..") 
-    console.info("Step 3: display interesting HTTP responses as we start the GET request");
-    console.log("statusCode: ", res.statusCode);
-    
-    console.log("\nheaders: ", res.headers);
-    console.info('\n==============================================')
+    console.info("debug - ..now we are in the GET request..\n");
 
+    console.log("debug - Step 3: Display HTTP statusCode: ", res.statusCode);
+    
+ 
+    console.log("\ndebug - Step 4: Display HTTP headers: ", res.headers);
+    console.info('\n==============================================');
+
+
+    console.info("debug - Step 5: Initialize some temp variables");
+    console.info("\n==============================================");
     buffer='';
     i = 0;
 
-    // on 'data' coming back from the response socket, put that into 'd' and then add to variable 'buffer'
+
+    // Perform HTTP GET request
     res.on('data', function(d) {
         
         // some debug statements to see things happen as they happen
-        console.info('\nincrement index = ' + i);
-        console.info('\nbuffer has text = ' + buffer);
-        console.info('\nThe socket d has text = ' + d + '\n');
+        //console.info("\nincrement index = " + i);
+        //console.info("\nbuffer has text = " + buffer);
+        //console.info("\nThe socket d has text = " + d);
 
         // now we put the string coming back from the api call into 'buffer' appending as more text arrives
         buffer += d.toString();
 
-        // 
-        console.log('buffer = ' + buffer + '\n');
+        // now print the updated content in variable 'buffer'
+        console.log('debug - buffer = ' + buffer + '\n');
         
         i++;
     });
+    
 
     res.on('end', function() {
-        console.info('GET result:\n');
+        console.info('debug - GET result:\n');
         console.log(buffer);
-        console.info('\n\nCall completed');
+	
+		var ex = '{ "id" : 1, "name" : "A green door", "price": 12 }';
+		
+		console.info('\ndebug - getting ready to parse');
+		//var parsedData = JSON.parse('{ "id":1, "name": "a green door", "price": 12 }');
+		//var parsedData = JSON.parse(ex);
+		var parsedData = JSON.parse(buffer);
+		
+		
+		console.info('\ndebug - dumping parsed data');
+		console.info(parsedData); 
+		console.info('\ndebug - results parsed.. ');
+		//console.info('\ndebug - now to print something');
+		//console.info('\n>> results = ' + parsedData.companies.name);
+        console.info('\n\ndebug - Call completed');
     });
+    
+
 
 });
 reqGet.on('error', function(e) {
